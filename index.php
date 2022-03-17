2de5d3ccaf368804a79cd81036f3214025a80d31
hemingchao15 <101722948+hemingchao15@users.noreply.github.com>
2022-03-17 09:46:22 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,183 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>食来运转</title>
+<link rel="stylesheet" type="text/css" href="Untitled-1.css" >
+</head>
+
+<body>
+<!--header 开始-->
+<div id="header">
+<!--logo 开始-->
+<div class="logo"><img src="images/logo.png" width="86" height="94" /></div>
+<!--logo 结束-->
+<!--nav 开始-->
+<div class="nav">
+<a href="index.html" class="ys">首页</a>
+<a href="brand.html" >品牌介绍</a>
+<a href="feature.html" >特色美食</a>
+<a href="stores.html" >门店展示</a>
+<a href="new.html" >新闻活动</a>
+<a href="zsjm.html" >招商加盟</a>
+<a href="#.html" >客户服务</a>
+<a href="#.html" >人力资源</a>
+<a href="#.html" >联系我们</a>
+</div>
+<!--nav 结束-->
+</div>
+<!--header 结束-->
+
+<!--banner 开始-->
+<div id="banner">
+<img src="images/index/banner.jpg" >
+</div>
+<!--banner 图结束-->
+
+<!--main 开始-->
+<div id="main">
+<!--tabnav 开始-->
+<div class="tabnav">
+<ul>
+<li class="current"><span>早餐本地化</span></li>
+<li><span>中餐快餐化</span></li>
+<li><span>晚餐特色化</span></li>
+</ul>
+</div>
+<!--tabnav 结束-->
+<!--tabcon 开始-->
+<div class="tabcon">
+<!--title 开始-->
+<div class="title">
+<img src="images/index/zaocan.jpg">
+</div>
+<!--title 结束-->
+<!--imglist 开始-->
+<div class="imglist">
+<ul>
+<li><img src="images/index/01.jpg"><p>营养卷饼</p></li>
+<li><img src="images/index/02.jpg"><p>美味养生</p></li>
+<li><img src="images/index/03.jpg"><p>养生鸡汤面</p></li>
+<li><img src="images/index/04.jpg"><p>可口小笼包</p></li>
+<li><img src="images/index/05.jpg"><p>香脆煎饼</p></li>
+<li><img src="images/index/06.jpg"><p>营养燕麦粥</p></li>
+<li><img src="images/index/07.jpg"><p>爽口卷菜饼</p></li>
+<li><img src="images/index/08.jpg"><p>香辣肉拌面</p></li>
+</ul>
+</div>
+<!--imglist 结束-->
+</div>
+<!--tabcon 结束-->
+</div>
+<!--main 结束-->
+
+<!--门店特色开始-->
+<div id="feature">
+<!--餐厅设计模块开始-->
+<div class="box left">
+<div class="top">
+<h1>餐厅设计</h1>
+<img src="images/index/icon1.png" width="159" height="91" />
+</div>
+<div class="bottom">
+<h2><span>0</span>设计费</h2>
+<h3>全程设计跟踪</h3>
+<p>快餐店内部的设计与布局既要考虑全局与部分间的和谐、均匀、对称，又要使设计表现出浓郁的风格情调，让客人一进快餐店在视觉和感觉上都能强烈地感受到形式美与艺术美，得到一种享受</p>
+</div>
+</div>
+<!--餐厅设计模块结束-->
+<!--厨师团队模块开始-->
+<div class="box center">
+<div class="top">
+<h1>厨师团队</h1>
+<img src="images/index/icon2.png" width="131" height="104" />
+</div>
+<div class="bottom">
+<h2><span>100</span>家门店</h2>
+<h3>厨师团队指导</h3>
+<p>我们会在开业前或更新时派遣厨务督导去分店对厨师进行培训，当然分店也可以派遣自己的厨师到总部来学习，总部会将这些厨师的学习报告发送给业主以便您了解他们的情况。</p>
+</div>
+</div>
+<!--厨师团队模块结束-->
+<!--管理经营模块开始-->
+<div class="box right">
+<div class="top">
+<h1>管理经营</h1>
+<img src="images/index/icon3.png" width="102" height="99" />
+</div>
+<div class="bottom">
+<h2><span>500</span>人团队</h2>
+<h3>经营管理支持</h3>
+<p>快餐店的开业指导，需要细化细节，进行详细指导。具体分为以下步骤。成立筹备小组，确定小组成员及分工，开始着手制定员工手册、规章制度、服务程序、岗位职责等。</p>
+</div>
+</div>
+<!--管理经营模块结束-->
+</div>
+<!--门店特色结束-->
+<!--门店展示开始-->
+<div id="show">
+<h1>门店展示</h1>
+<ul>
+<li><img src="images/index/1.jpg" /></li>
+<li><img src="images/index/2.jpg" /></li>
+<li><img src="images/index/3.jpg" /></li>
+<li><img src="images/index/4.jpg" /></li>
+</li>
+</div>
+<!--门店展示结束-->
+<!--资讯区块开始-->
+<div id="news">
+<!--news-left 开始-->
+<div class="news-left">
+<h1>美食资讯</h1>
+<!--left 开始-->
+<div class="left">
+<p>中式快餐逐渐成为餐饮行业主流快餐·····</p>
+</div>
+<!--left 结束-->
+<!--right 开始-->
+<div class="right">
+<ul>
+<li><a href="#">一店多餐多元化经营模式引领餐······</a></li>
+<li><a href="#">连锁中式快餐加盟企业如何做大做······</a></li>
+<li><a href="#">休闲餐市场看好快餐放慢脚步全面······</a></li>
+<li><a href="#">推出宵夜下午茶进军休闲餐饮市场······</a></li>
+<li><a href="#">大众餐饮引潮流中式快餐渐成餐饮······</a></li>
+<li><a href="#">大众餐饮引潮流中式快餐渐成餐饮······</a></li>
+<li><a href="#">大众餐饮引潮流中式快餐渐成餐饮······</a></li>
+</ul>
+</div>
+<!--right 结束-->
+</div>
+<!--news-left 结束-->
+<!--new-right 开始-->
+<div class="news-right">
+<h1>联系加盟</h1>
+<div class="conten">
+<p>食来运转餐饮管理有限公司成立于2012年，作为首家全国连锁中式自选快餐品牌，在中式快餐业首次提出了连锁餐饮菜品本地化运营的模式，实现了连锁快餐区域化发展，成为中国发展最为迅速的中式连锁快餐。</p>
+</div>
+</div>
+<!--new-right 结束-->
+</div>
+<!--资讯区块结束-->
+<!--底部导航nav开始-->
+<div id="foot-nav">
+<a href="#">首页</a>
+<a href="#">品牌介绍</a>
+<a href="#">特色美食</a>
+<a href="#">门店展示</a>
+<a href="#">新闻活动</a>
+<a href="#">招商加盟</a>
+<a href="#">客户服务</a>
+<a href="#">人力资源</a>
+<a href="#">联系我们</a>
+</div>
+<!--底部导航nav结束-->
+<!--copyRight 开始-->
+<div id="copyRight">
+<p>地址：xxxxxxxx    邮箱：xxxxxxxx    电话：xxxxxxxxx</br>
+copyRight&copy;2016xxxxxx    ICP 备 xxxxx</p>
+</div>
+<!--copyRight 结束-->
+</body>
+</html>
